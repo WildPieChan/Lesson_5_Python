f34213a6a4b8ea925e8c9847342a8f6f9f8be51b
Kuksa <kirkfanton.kb@gmail.com>
2022-11-18 12:49:37 +0300

    DEBUG first task

--- Program_1.py
+++ Program_1.py
@@ -1,8 +1,19 @@
-def WordsDeleting(someText):
+def WordsDeleting1(someText):
     someText = list(filter(lambda x: 'абв' not in x, someText.split()))
+    return " ".join(someText)
+
+# Teacher's function to save punctuation marks
+def WordsDeleting2(someText):
+    someText = someText.split()
+    for i in range(len(someText)):
+        if 'абв' in someText[i]:
+            if not someText[i].isidentifier():
+                someText[i] = someText[i][-1]
+            else:
+                someText[i] = ''
     return " ".join(someText)
 
 print('Program will delete any words with "абв".')
 text = str(input("Enter any text: "))
-text = WordsDeleting(text)
+text = WordsDeleting1(text)
 print(text)
